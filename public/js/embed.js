12a13b200ab3329c47f7c465423af4645fc2f9e0
Ralph John Galindo <ralphjesy@gmail.com>
2017-01-14 08:03:03 +0800

    Trying Relative Protocol for Insecure Content

--- public/js/embed.js
+++ public/js/embed.js
@@ -1,15 +1,11 @@
-function sendHeight()
-{
+window.onload = function(){
+    var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+    var eventer = window[eventMethod];
+    var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+
     if(parent.postMessage)
     {
         var height = document.body.scrollHeight;
         parent.postMessage(height, '*');
     }
-}
-
-window.onload = function(){
-    var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-    var eventer = window[eventMethod];
-    var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
-    sendHeight();
 }
